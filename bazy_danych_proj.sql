acb3528a75ef4d1bcad10076bafac411185843d9
Julia <102248096+JustJuulia@users.noreply.github.com>
2024-01-03 14:53:13 +0100

    Made databse for project
    everything will be explained in description

---
+++ bazy_danych_proj.sql
@@ -0,0 +1,205 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sty 03, 2024 at 02:51 PM
+-- Wersja serwera: 10.4.28-MariaDB
+-- Wersja PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bazy_danych_proj`
+--
+CREATE DATABASE IF NOT EXISTS `bazy_danych_proj` DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci;
+USE `bazy_danych_proj`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `classes`
+--
+
+DROP TABLE IF EXISTS `classes`;
+CREATE TABLE `classes` (
+  `id_classes` int(11) NOT NULL,
+  `class_name` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `hours`
+--
+
+DROP TABLE IF EXISTS `hours`;
+CREATE TABLE `hours` (
+  `id_hours` int(11) NOT NULL,
+  `hour_start` time NOT NULL,
+  `hour_end` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `main`
+--
+
+DROP TABLE IF EXISTS `main`;
+CREATE TABLE `main` (
+  `id` int(11) NOT NULL,
+  `weekdays_id` int(11) NOT NULL,
+  `hours_id` int(11) NOT NULL,
+  `subjects_id` int(11) NOT NULL,
+  `classes_id` int(11) NOT NULL,
+  `rooms_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `rooms`
+--
+
+DROP TABLE IF EXISTS `rooms`;
+CREATE TABLE `rooms` (
+  `id_rooms` int(11) NOT NULL,
+  `room_num` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `subjects`
+--
+
+DROP TABLE IF EXISTS `subjects`;
+CREATE TABLE `subjects` (
+  `id_subjects` int(11) NOT NULL,
+  `subject_name` varchar(115) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `weekdays`
+--
+
+DROP TABLE IF EXISTS `weekdays`;
+CREATE TABLE `weekdays` (
+  `id_weekdays` int(11) NOT NULL,
+  `weekday` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`id_classes`);
+
+--
+-- Indeksy dla tabeli `hours`
+--
+ALTER TABLE `hours`
+  ADD PRIMARY KEY (`id_hours`);
+
+--
+-- Indeksy dla tabeli `main`
+--
+ALTER TABLE `main`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `weekdays_id` (`weekdays_id`),
+  ADD KEY `hours_id` (`hours_id`),
+  ADD KEY `subjects_id` (`subjects_id`),
+  ADD KEY `classes_id` (`classes_id`),
+  ADD KEY `rooms_id` (`rooms_id`);
+
+--
+-- Indeksy dla tabeli `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id_rooms`);
+
+--
+-- Indeksy dla tabeli `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id_subjects`);
+
+--
+-- Indeksy dla tabeli `weekdays`
+--
+ALTER TABLE `weekdays`
+  ADD PRIMARY KEY (`id_weekdays`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `id_classes` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `hours`
+--
+ALTER TABLE `hours`
+  MODIFY `id_hours` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `main`
+--
+ALTER TABLE `main`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `id_rooms` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `id_subjects` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `weekdays`
+--
+ALTER TABLE `weekdays`
+  MODIFY `id_weekdays` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `main`
+--
+ALTER TABLE `main`
+  ADD CONSTRAINT `main_ibfk_1` FOREIGN KEY (`weekdays_id`) REFERENCES `weekdays` (`id_weekdays`),
+  ADD CONSTRAINT `main_ibfk_2` FOREIGN KEY (`hours_id`) REFERENCES `hours` (`id_hours`),
+  ADD CONSTRAINT `main_ibfk_3` FOREIGN KEY (`subjects_id`) REFERENCES `subjects` (`id_subjects`),
+  ADD CONSTRAINT `main_ibfk_4` FOREIGN KEY (`classes_id`) REFERENCES `classes` (`id_classes`),
+  ADD CONSTRAINT `main_ibfk_5` FOREIGN KEY (`rooms_id`) REFERENCES `rooms` (`id_rooms`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
