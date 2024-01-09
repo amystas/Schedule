6511b98c23193aa7ac2069f35ca5374396cd84a9
Julia <102248096+JustJuulia@users.noreply.github.com>
2024-01-09 15:09:47 +0100

    nearly sent everything to database
    ERROR IN LINE 135-140 the hour_end aint editing 😭

--- getDataFromWebsite.php
+++ getDataFromWebsite.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 @include("functions.php");
 $pagelink = "http://www.sci.edu.pl/cwicz/plan.html";
 $pageHTML = file_get_contents($pagelink);
@@ -30,5 +30,134 @@ foreach ($cells as $cell) {
     $data[] = $cell->nodeValue;
 }
 
-print_r($data);
+
+
+// dni tygodnia :D
+$indexesToExtractforweekdays = array(2, 3, 4, 5, 7, 8, 9);
+
+$weekdays = array();
+foreach ($indexesToExtractforweekdays as $index) {
+    if (isset($data[$index])) {
+        $weekdays[] = $data[$index];
+    }
+}
+//print_r($weekdays);
+
+// godziny lekcyjne >:P
+$indexesToExtractforhours = array(12, 23, 34, 56, 67, 78, 89, 100, 111, 122, 133, 144);
+
+$studyhours = array();
+foreach ($indexesToExtractforhours as $index) {
+    if (isset($data[$index])) {
+        $studyhours[] = $data[$index];
+    }
+}
+//print_r($studyhours);
+
+// podzielenie liczb na hour start i end zeby ladnie do bazy to wchodzilo
+$hour_start = [];
+$hour_end = [];
+foreach ($studyhours as $rekord) {
+    $hour_start[] = substr($rekord, 0, 5);
+    $hour_end[] = substr($rekord, 8, 13);
+}
+
+//print_r($hour_start);
+//print_r($hour_end);
+
+// przedmioty, klasy, sale ^.^
+$indexesToExtractforsubjects = array(58, 68, 69, 73, 79, 80, 84, 90, 91, 95, 101, 102, 106, 112, 113, 123, 124,);
+
+$threeinonearraywithsubj = array();
+foreach ($indexesToExtractforsubjects as $index) {
+    if (isset($data[$index])) {
+        $threeinonearraywithsubj[] = $data[$index];
+    }
+}
+//print_r($threeinonearraywithsubj);
+
+// dzielenie tego na sale, klasy, przedmioty
+
+$subj = [];
+$classes = [];
+$room = [];
+
+foreach ($threeinonearraywithsubj as $rekord) {
+    $typ = strtoupper($rekord[0]);
+    if ($typ === 'B') {
+        $subj[] = substr($rekord, 0, 11);
+        $classes[] = substr($rekord, 13, 10);
+        $room[] = substr($rekord, 23);
+    } elseif ($typ === 'S') {
+        $subj[] = substr($rekord, 0, 18);
+        $classes[] = substr($rekord, 20, 10);
+        $room[] = substr($rekord, 30);
+    }
+}
+//usuniecie (liczba) z klas
+
+//usuniecie duplikacji
+$uniqueSubj = array_unique($subj);
+$uniqueClass = array_unique($classes);
+$uniqueRoom = array_unique($room);
+//print_r($uniqueSubj);
+
+//print_r($uniqueClass);
+
+//print_r($uniqueRoom);
+//print_r($data);
+
+// WYSYLANIE DO SQL 
+
+try {
+    $link = mysqli_connect("localhost", "root", "", "bazy_danych_proj");
+} catch (Exception) {
+    echo 'no julka tak wlasnie potrafisz si epolaczyc z baza danych super normalnie wowzabowza :||||';
+}
+
+if ($link) {
+    // dni tygodnia
+    foreach ($weekdays as $weekday) {
+        $sql = "INSERT INTO `weekdays` (`id_weekdays`, `weekday`) VALUES (NULL, '$weekday')";
+        mysqli_query($link, $sql);
+    }
+
+
+    // godziny lekcyjne
+    $i = 0;
+    foreach ($hour_start as $hourst) {
+        $sql = "INSERT INTO `hours` (`id_hours`, `hour_start`, `hour_end`) VALUES ('$i', '$hourst', '$hour_end[$i]')";
+        $i++;
+        mysqli_query($link, $sql);
+    }
+    // naprawa hour end nwm czemu dziala ale mozna to zawsze po prostu zedytowac ;P
+    $i = 0;
+    foreach ($hour_end as $houren) {
+        $sql = "UPDATE `hours` SET hour_end = '$houren' WHERE id_hours = '$i'";
+        //nie dziala wtfwtf
+        $i++;
+        mysqli_query($link, $sql);
+    }
+
+    // klasy, np. 3C
+    foreach ($uniqueClass as $class) {
+        $sql = "INSERT INTO `classes` (`id_classes`, `class_name`) VALUES (NULL, '$class')";
+        mysqli_query($link, $sql);
+    }
+
+
+    // sale
+    foreach ($uniqueRoom as $room_v) {
+        $sql = "INSERT INTO `rooms` (`id_rooms`, `room_num`) VALUES (NULL, '$room_v')";
+        mysqli_query($link, $sql);
+    }
+
+
+    // przedmioty
+    foreach ($uniqueSubj as $subject) {
+        $sql = "INSERT INTO `subjects` (`id_subjects`, `subject_name`) VALUES (NULL, '$subject')";
+        mysqli_query($link, $sql);
+    }
+}
+
 ?>
